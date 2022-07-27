486fbe58ca7675b48cb3b052cba74c7af1ddea23
AssisTENT <maksim.aleksandr2000@mail.ru>
2022-07-27 17:36:15 +0300

    Update xamdiffs.py

--- xamdiffs.py
+++ xamdiffs.py
@@ -2,8 +2,8 @@
 """
 Usage: {prog} [OPTION] FILE1 FILE2
 OPTIONS
-    --amdiff		Compare two AndroidManifests, ignoring element and attribute order.
-
+    --amdiff		Compares two AndroidManifests, ignoring element and attribute order.
+    --resdiff		Requires apktool, Disassembles resources and searches for changes in them with keyword list
 """
 import sys
 import os
@@ -15,6 +15,8 @@ from androguard.misc import AnalyzeAPK
 import androguard
 
 NAME = '{http://schemas.android.com/apk/res/android}name'
+KEYWORDLIST = ["host", "api", "token", "web"]
+EXCLUDEDTYPES = ["image", "video", "media"]
 new_attribs=[]
 new_nodes=[]
 old_attribs=[]
@@ -141,16 +143,124 @@ def listdiffs(file1, file2):
             print("\nDeleted", node_name(node[0]), "in", node[1])
 #            print(write_nodes(node[0]), "in", node[1])
 #            print(node[1] + ":\n" + node[0])
+I=0
+def counti():
+    global I
+    I=I+1
+    print(I)
 
-def resdiff(dx1,dx2):
-    from filecmp import dircmp
+def diffbykey(filename1,filename2):
+    f1 = open(filename1, "r").readlines()
+    f2 = open(filename2, "r").readlines()
+    file_1_line = f1.readline()
+    file_2_line = f2.readline()
+    while file_1 != '' or file_2_line != '':
+  
+        # Removing whitespaces
+        file_1_line = file_1_line.rstrip()
+        file_2_line = file_2_line.rstrip()
+  
+        # Compare the lines from both file
+        if file_1_line != file_2_line and any(word in line for word in KEYWORDLIST):
+            # otherwise output the line on file1 and use @ sign
+            if file_1_line == '':
+                print("@", "Line-%d" % line_no, file_1_line)
+            else:
+                print("@-", "Line-%d" % line_no, file_1_line)
+              
+            # otherwise output the line on file2 and use # sign
+            if file_2_line == '':
+                print("#", "Line-%d" % line_no, file_2_line)
+            else:
+                print("#+", "Line-%d" % line_no, file_2_line)
+  
+            # Print a empty line
+            print()
+  
+        # Read the next line from the file
+        file_1_line = file_1.readline()
+        file_2_line = file_2.readline()
+  
+        line_no += 1
+    f1.close()
+    f2.close()
+
+def printwithkeyword(line):
+    for word in KEYWORDLIST:
+        index = line.find(word)
+        if index == -1:
+            continue
+        line = line[:index] + '\x1b[6;31;40m' + line[index:index+len(word)] + '\x1b[0m' + line[index+len(word):]
+    print(line)
+
+def resdiff(dir1,dir2):
+    import filecmp
+    import difflib
+    import mimetypes
     def print_diff_files(dcmp):
+#        print(filecmp.cmpfiles(dcmp.left,dcmp.right,dcmp.common_files, shallow=False)[1], end=' ')
         for name in dcmp.diff_files:
-            print("diff_file %s found in %s and %s" % (name, dcmp.left,
-                  dcmp.right))
+            path1 = os.path.join(dcmp.left,name)
+            path2 = os.path.join(dcmp.right,name)
+            if all(word not in str(mimetypes.guess_type(path1)[0]) for word in EXCLUDEDTYPES):
+#                if name != "AndroidManifest.xml":
+                    try:
+                        f1 = open(path1, "r").readlines()
+                        f2 = open(path2, "r").readlines()
+#                        diffbykey(path1,path2)
+                        isdiffprint=0
+                        for line in difflib.unified_diff(f1,f2, n=0):
+                            # public.xml only initializes ids
+                            if any(word in line.lower() for word in KEYWORDLIST) and name!="public.xml":
+                                if isdiffprint == 0:
+                                    print("\ndiff_file %s found in %s and %s" % (name, dcmp.left, dcmp.right))
+                                    counti()
+                                    isdiffprint=1
+                                printwithkeyword(line)
+                    except UnicodeDecodeError:
+                        pass
+ #               else:
+ #                   listdiffs(lxml.etree.parse(path1).getroot(), lxml.etree.parse(path2).getroot())
         for sub_dcmp in dcmp.subdirs.values():
-            print_diff_files(sub_dcmp)
-    dcmp = dircmp('dir1', 'dir2')
+            if "/tmp/apk1/smali" not in os.path.abspath(sub_dcmp.left) and "/tmp/apk1/original" not in os.path.abspath(sub_dcmp.left):
+#            if "/tmp/apk1/assets" in os.path.abspath(sub_dcmp.left) or "/tmp/apk1/META-INF" in os.path.abspath(sub_dcmp.left) or "/tmp/apk1/lib" in os.path.abspath(sub_dcmp.left):
+                print_diff_files(sub_dcmp)
+        for name in dcmp.left_only:
+            path1 = os.path.join(dcmp.left,name)
+            if all(word not in str(mimetypes.guess_type(path1)[0]) for word in EXCLUDEDTYPES):
+                try:
+                    file = open(os.path.join(dcmp.left,name), "r").readlines()
+                    isdiffprint=0
+                    for line in file:
+                        if any(word in line.lower() for word in KEYWORDLIST):
+                            if isdiffprint == 0:
+                                print("\nleft_only %s found in %s" % (name, dcmp.left))
+                                counti()
+                                isdiffprint=1
+                            printwithkeyword(line)
+                except UnicodeDecodeError:
+                    print("left_only binary %s found in %s" % (name, dcmp.left))
+                    counti()
+                except IsADirectoryError:
+                    print("left_only directory %s found in %s" % (name, dcmp.left))
+                    counti()
+        for name in dcmp.right_only:
+            path1 = os.path.join(dcmp.left,name)
+            if all(word not in str(mimetypes.guess_type(path1)[0]) for word in EXCLUDEDTYPES):
+                try:
+                    file = open(os.path.join(dcmp.right,name), "r").readlines()
+                    isdiffprint=0
+                    for line in file:
+                        if any(word in line.lower() for word in KEYWORDLIST):
+                            if isdiffprint == 0:
+                                print("\nright_only %s found in %s" % (name, dcmp.right))
+                                counti()
+                                isdiffprint=1
+                            printwithkeyword(line)
+                except UnicodeDecodeError:
+                    print("right_only binary %s found in %s" % (name, dcmp.right))
+                    counti()
+    dcmp = filecmp.dircmp(dir1, dir2)
     print_diff_files(dcmp) 
 
 def print_usage(prog):
@@ -173,6 +283,10 @@ if __name__ == '__main__':
     # check if files have same extension and if its .apk
     if os.path.splitext(file1)[1]==os.path.splitext(file2)[1]==".apk":
         if '--resdiff' in args:
+            subprocess.call(['apktool','d',file1,'-f','-o','/tmp/apk1'], stderr=subprocess.STDOUT)
+            subprocess.call(['apktool','d',file2,'-f','-o','/tmp/apk2'], stderr=subprocess.STDOUT)
+            exit(resdiff("/tmp/apk1","/tmp/apk2"))
+        if '--enumdiff' in args:
             s = session.Session()
             with open(file1, "rb") as fd:
                 s.add(file1, fd.read())
@@ -181,7 +295,7 @@ if __name__ == '__main__':
             with open(file2, "rb") as fd:
                 s.add(file2, fd.read())
             export_apps_to_format(file2, s, "/tmp/apkdiffer2")
-            exit(resdiff("/tmp/apkdiffer1","/tmp/apkdiffer2"))
+#            exit(resdiff("/tmp/apkdiffer1","/tmp/apkdiffer2"))
         a1, d1, dx1 = AnalyzeAPK(file1)
         a2, d2, dx2 = AnalyzeAPK(file2)
         xml1 = a1.get_android_manifest_xml()
